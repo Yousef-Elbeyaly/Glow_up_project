3c4fc6dc4ec014ff3b2f7a6518c3403f7fc65f49
Yousef-Elbeyaly <yousef.amir080@gmail.com>
2026-05-04 17:56:15 +0300

    setup

--- import pandas as pd.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-# السحر كله في كلمة encoding='utf-8-sig'
-df = pd.read_csv('sales_data.csv', encoding='utf-8-sig')
-
-# كدة الأسامي هتظهر لك بالعربي "فاتيكا" و "ديرما رول" زي ما كانت في SQL
-print(df.head())
